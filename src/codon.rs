04a34fbde3be763994dabe740d839c843a169e4b
Will Johnston <wbjohnston@gmail.com>
2017-09-22 02:08:38 -0400

    FIRST BLOOD

---
+++ src/codon.rs
@@ -0,0 +1,14 @@
+use std::cmp::PartialOrd;
+
+pub trait Organism
+{
+    /// Randomly mutate this organism
+    fn mutate(&self) -> Self;
+
+    /// Cross this orgnanism with another
+    fn cross(&self, other: &Self) -> Self;
+
+    /// Get the fitness of the organism
+    fn fitness<T>(&self) -> T
+        where T: PartialOrd;
+}
