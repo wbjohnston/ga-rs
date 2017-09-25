481acb72196d1876b00f6c3e89be82e50079a12f
Will Johnston <wbjohnston@gmail.com>
2017-09-25 15:04:49 -0400

    Implemented starter GA, (don't use)
    I've implemented a partial implemntation of the genetic algorithm. Right
    now it's missing the mutation and crossover parts of the algorithm --
    making it useless. I'm publishing the crate on this commit

---
+++ src/genome.rs
@@ -0,0 +1,29 @@
+//! A trait that lets us operate on structures, treating them like a "genome"
+//! so we can "mutate" and "cross" them with other structures
+
+use rand::Rng;
+
+pub trait Genome
+{
+    /// Create an entirely random `Genome`
+    fn genesis<R>(rng: &mut R) -> Self
+        where R: Rng;
+
+    /// Randomly mutate a single "gene" in the genome
+    ///
+    /// # Return
+    /// TODO
+    fn mutate<R>(&self, rng: &mut R) -> Self
+        where R: Rng;
+
+    /// Cross this orgnanism with another
+    ///
+    /// # Arguments
+    /// * `other`: the "mate" of this organism that will cross genomes
+    fn cross<R>(&self, other: &Self, rng: &mut Rng) -> Self
+        where R: Rng;
+
+    /// Get the fitness of the organism
+    fn fitness<T>(&self) -> T
+        where T: Ord;
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,109 @@
 //! Genetic algorithm tools
 
-mod organism;
-pub use self::organism::Organism;
+extern crate rand;
+use rand::Rng;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+extern crate num;
+use num::traits::Float;
+
+mod genome;
+pub use self::genome::Genome;
+
+/// Genetic algorithm
+///
+/// # Arguments
+/// * `pop_size`: number of individuals in the population
+/// * `n_iter`: number of iterations to run algorithm
+/// * `replace_rate`: rate at which individuals will be repleaced with results
+///     of crossover
+/// * `mut_rate`: rate of mutation in individuals
+/// * `rng`: Random number generator that will be used to get randomness
+///
+/// # Return
+/// TODO
+pub fn genetic_algorithm<G, R, O, F>(
+    pop_size:     usize,
+    n_iter:       usize,
+    replace_rate: F,
+    mutate_rate:  F,
+    mut rng:      R
+    ) -> Vec<(G, O)>
+where G: Genome + Clone, // TODO, see if `Clone` this is really necessary
+      R: Rng,
+      O: Ord,
+      F: Float
+{
+    // initial population, zipped with fitness and sorted
+    let mut pop = init_generation(pop_size, &mut rng);
+
+    // run survival of the fittest simulation
+    for _ in 0..n_iter {
+        pop = next_generation(pop, replace_rate, mutate_rate, &mut rng);
     }
+
+    pop
 }
+
+/// Create an initial generation of specified size
+///
+/// # Arguments
+/// * `size`: size of population
+/// * `rng`: Random number generator to pull randomness from
+///
+/// # Return
+/// TODO
+fn init_generation<G, R, O>(size: usize, rng: &mut R) -> Vec<(G, O)>
+where G: Genome,
+      R: Rng,
+      O: Ord
+{
+    let mut gen: Vec<(G, O)> = Vec::with_capacity(size);
+    for _ in 0..size {
+        let indv = G::genesis(rng);
+        let fitness = indv.fitness();
+        gen.push((indv, fitness));
+    }
+
+    gen.sort_by(|a, b| a.1.cmp(&b.1));
+    gen
+}
+
+/// Create the next generation
+///
+/// # Arguments
+/// * `gen`: TODO
+/// * `replace_rate`: TODO
+/// * `mutate_rate`: TODO
+/// * `rng`: TODO
+///
+/// # Return
+/// TODO
+fn next_generation<G, R, O, F>(
+    gen:          Vec<(G, O)>,
+    replace_rate: F,
+    mutate_rate:  F,
+    rng:          R
+    ) -> Vec<(G, O)>
+where G: Genome + Clone,
+      R: Rng,
+      O: Ord,
+      F: Float
+{
+    let mut new_gen: Vec<G> = Vec::with_capacity(gen.len());
+
+    // let highest_idx = ((1. - replace_rate) * new_gen.len()) as usize;
+    // for i in 0..highest_idx {
+    //     new_gen[i] = gen[i].0.clone();
+    // }
+    
+    // TODO: crossover
+
+    // TODO: mutation
+
+    new_gen.into_iter()
+        .map(|x| {
+            let fitness = x.fitness();
+            (x, fitness)
+        }).collect()
+}
+
