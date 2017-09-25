844a8f1cd4b09a2478380a3a2539fdf0f6dde512
Will Johnston <wbjohnston@gmail.com>
2017-09-25 16:55:54 -0400

    Added Codon trait and Genome::codon_iter
    I want to make a default implementation for crossover, so I've created
    the trait codon that describes "how" to mutate a single part of the
    genome

--- src/genome.rs
+++ src/genome.rs
@@ -1,12 +1,34 @@
 //! A trait that lets us operate on structures, treating them like a "genome"
 //! so we can "mutate" and "cross" them with other structures
 
+use std::iter::Iterator;
+
 use rand::Rng;
+
+/// A single unit of mutation
+pub trait Codon
+{
+    /// Mutate the codon randomly
+    fn mutate<R>(&self, rng: &mut R) -> Self
+        where R: Rng;
+
+    /// Mutate the codon in place randomly
+    fn mutate_mut<R>(&mut self, rng: &mut R)
+        where R: Rng;
+}
 
 /// Interface for randomly mutating, crossing over, and creating a genome used
 /// in genetic algorithms
 pub trait Genome
 {
+    /// Immutabel codon iterator
+    fn codon_iter<C>(&self) -> Iterator<Item=C>
+        where C: Codon;
+
+    /// Mutable Codon iterator
+    fn codon_iter_mut<C>(&mut self) -> Iterator<Item=&mut C>
+        where C: Codon;
+
     /// Create an entirely random `Genome`
     fn genesis<R>(rng: &mut R) -> Self
         where R: Rng;
@@ -15,6 +37,8 @@ pub trait Genome
     fn mutate<R>(&self, rate: f32, rng: &mut R) -> Self
         where R: Rng;
 
+    // TODO: cross can be implemented entirely using the codon iterator methods,
+    // providing a default implementation
     /// Cross this orgnanism with another
     ///
     /// # Arguments
