87069dc4e8fe229ffcda8188559801467104c03a
Will Johnston <wbjohnston@gmail.com>
2017-10-02 13:12:45 -0400

    stubbed tree gp module

--- src/primitives/tree/mod.rs
+++ src/primitives/tree/mod.rs
@@ -1,1 +1,85 @@
 //! Tree based genetic programming primitives
+
+use rand::Rng;
+
+use traits::{Chromosome, Genome};
+
+/*******************************************************************************
+* Notes:
+*   Possibly rewrite the `genome::genesis` and mutation function to take in a 
+*   "strategy" field instead of having multiple types sharing the same fields
+*******************************************************************************/
+/// Binary tree with fixed size
+pub struct FixedBinaryTree<C>
+where
+    C: Chromosome
+{
+    value: C,
+    left: Option<Box<C>>,
+    right: Option<Box<C>>
+}
+
+impl<C> Genome for FixedBinaryTree<C>
+where
+    C: Chromosome
+{
+    fn genesis(rng: &mut Rng) -> Self
+    {
+        unimplemented!();
+    }
+
+    fn mutate(&self, rate: f32, rng: &mut Rng) -> Self
+    {
+        unimplemented!();
+    }
+
+    fn cross(&self, other: &Self, rng: &mut Rng) -> Self
+    {
+        unimplemented!();
+    }
+
+    fn fitness<O>(&self) -> O
+    where
+        O: Ord
+    {
+        unimplemented!();
+    }
+}
+
+/// Binary tree with non-fixed size and non-fixed depth mutation strategy
+pub struct VariableBinaryTree<C>
+where
+    C: Chromosome
+{
+    value: C,
+    left: Option<Box<C>>,
+    right: Option<Box<C>>
+}
+
+impl<C> Genome for VariableBinaryTree<C>
+where
+    C: Chromosome
+{
+    fn genesis(rng: &mut Rng) -> Self
+    {
+        unimplemented!();
+    }
+
+    fn mutate(&self, rate: f32, rng: &mut Rng) -> Self
+    {
+        unimplemented!();
+    }
+
+    fn cross(&self, other: &Self, rng: &mut Rng) -> Self
+    {
+        unimplemented!();
+    }
+
+    fn fitness<O>(&self) -> O
+    where
+        O: Ord
+    {
+        unimplemented!();
+    }
+}
+
