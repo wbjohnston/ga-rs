7c7a9aaf36fe8cce17b75a2c3ed8ce34b402571f
Will Johnston <wbjohnston@gmail.com>
2017-10-03 18:26:41 -0400

    Changed Operation enum to return type, !container

--- src/primitives/tgp/mod.rs
+++ src/primitives/tgp/mod.rs
@@ -7,25 +7,17 @@ use std::collections::{HashSet, VecDeque};
 pub enum Primitive<T>
 {
     Constant(T),
+
     EphemeralConstant(fn(&mut Rng) -> T),
+
     Operation {
         arity: usize,
-        op: fn(Vec<Primitive<T>>) -> Primitive<T>
+        op: fn(Vec<T>) -> T,
     }
 }
 
 pub struct Tree<T>
 {
-    value: Primitive<T>,
+    data: Primitive<T>,
     children: Vec<Box<Tree<T>>>
-}
-
-impl<T> Tree<T>
-{
-}
-
-/// Compile a tree containing ephemeral constants into an evaluated tree
-pub fn compile<T>(a: &Tree<T>) -> Tree<T>
-{
-    unimplemented!();
 }
