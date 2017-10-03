c969368ee5ce4112b1f339a6c6a8d36edb363d31
Will Johnston <wbjohnston@gmail.com>
2017-10-03 17:39:10 -0400

    Started writing tree primitives

--- src/primitives/tgp/mod.rs
+++ src/primitives/tgp/mod.rs
@@ -1,2 +1,31 @@
 //! Tree based genetic programming primitives
 
+use rand::Rng;
+
+use std::collections::{HashSet, VecDeque};
+
+pub enum Primitive<T>
+{
+    Constant(T),
+    EphemeralConstant(fn(&mut Rng) -> T),
+    Operation {
+        arity: usize,
+        op: fn(Vec<Primitive<T>>) -> Primitive<T>
+    }
+}
+
+pub struct Tree<T>
+{
+    value: Primitive<T>,
+    children: Vec<Box<Tree<T>>>
+}
+
+impl<T> Tree<T>
+{
+}
+
+/// Compile a tree containing ephemeral constants into an evaluated tree
+pub fn compile<T>(a: &Tree<T>) -> Tree<T>
+{
+    unimplemented!();
+}
--- src/traits/gp/genome.rs
+++ src/traits/gp/genome.rs
@@ -2,12 +2,15 @@
 
 use petgraph::Graph;
 
-/// Interface for randomly mutating, crossing over, and creating a genome used
-/// in genetic algorithms
-pub trait Genome: From<Graph> + Into<Graph>
-{
-    /// Get the fitness of the organism
-    fn fitness<O>(&self) -> O
-    where
-        O: Ord;
-}
+pub trait Genome
+{}
+
+// Interface for randomly mutating, crossing over, and creating a genome used
+// in genetic algorithms
+// pub trait Genome: From<Graph> + Into<Graph>
+// {
+//     /// Get the fitness of the organism
+//     fn fitness<O>(&self) -> O
+//     where
+//         O: Ord;
+// }
